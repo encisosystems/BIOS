cee9b3199909fa19dff9325c3e4ad8f227b10bb9
dpalacios <diferpaho@gmail.com>
2021-05-15 08:31:31 -0500

    add footer model

--- cms/models.py
+++ cms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class footer(models.Model):
+company = models.CharField(max_length=30)
+year = models.CharField(max_length=30)
+phone = models.CharField(max_length=30)
+mail = models.CharField(max_length=30)
