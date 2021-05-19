82b584fddb93248d468113ccebed3a399ca5b23d
LinaMejia <linalmejia@gmail.com>
2021-05-19 18:01:19 -0500

    Create model Template in App cms

--- cms/models.py
+++ cms/models.py
@@ -7,3 +7,22 @@ class footer(models.Model):
     year = models.CharField(max_length=30)
     phone = models.CharField(max_length=30)
     mail = models.CharField(max_length=30)
+
+
+class Template(models.Model):
+    icon = models.ImageField(
+        upload_to='cms/icons',
+    )
+    logo = models.ImageField(
+        upload_to='cms/logos',
+    )
+    header_title = models.CharField(
+        max_length=50
+    )
+
+    def __str__(self):
+        return f'{self.header_title}'
+
+    class Meta:
+        verbose_name = 'Template'
+        verbose_name_plural = 'Templates'
