484005d32680a4f13a4fce9fec2227288c5c00d6
Alexis Acevedo <badzdiaz@gmail.com>
2021-05-19 23:12:49 -0500

    add model Carrousel and Image from cms

--- cms/models.py
+++ cms/models.py
@@ -7,3 +7,12 @@ class footer(models.Model):
     year = models.CharField(max_length=30)
     phone = models.CharField(max_length=30)
     mail = models.CharField(max_length=30)
+
+class Carrousel(models.Model):
+    title = models.CharField(max_length=60)    
+
+class Image(models.Model):
+    title = models.CharField(max_length=60)
+    alt = models.CharField(max_length=60)
+    image = models.FileField(upload_to='images/')
+
