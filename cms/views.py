d87ef283939ba63700e3a7254902789d5d2d863d
Paula Velasquez <p.andreaveca@gmail.com>
2021-05-20 19:34:55 -0500

    Blog and blog details page added. Fixing problem of the menu links

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def blog(request):
+    return render(request, "blog.html")
+
+def blogDetails(request):
+    return render(request, "blog-details.html")
