a2c6c8dcafa1255c2a1ad575a3986c042d1f6e4a
Paula Velasquez <p.andreaveca@gmail.com>
2021-05-22 11:00:48 -0500

    Add cma carnet to the views and menu

--- certificates/views.py
+++ certificates/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def medicalCertificate(request):
     return render(request, "medical-certificate.html")
+
+def carnet(request):
+    return render(request, "cma-carnet.html")
