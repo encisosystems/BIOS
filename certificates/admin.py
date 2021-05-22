c11994f38cd6749092a16e734bfc7bb8b9c18107
William Torres <wiltorc2430@gmail.com>
2021-05-22 11:10:57 -0500

    Add Admin Models
    Add admin models and Fix Verbose Names

--- certificates/admin.py
+++ certificates/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(TypeDocument)
+admin.site.register(City)
+admin.site.register(TypeTest)
+admin.site.register(Test)
+admin.site.register(ConcepType)
+admin.site.register(Person)
+admin.site.register(Official)
+admin.site.register(Doctor)
+admin.site.register(ClientEntity)
+admin.site.register(Manipulator)
+admin.site.register(Assistance)
+admin.site.register(CertificateAssistance)
+admin.site.register(Capacitation)
+admin.site.register(Entity)
+admin.site.register(CapacityCertificate)
+admin.site.register(CertificateDoctor)
