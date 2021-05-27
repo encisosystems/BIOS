b0d905aea8a50ad3a2097b4b321fedd01a0d44c0
Michael Pardo <michael.pardo@unillanos.edu.co>
2021-05-26 23:02:37 -0500

    add context processors for get footer description list

--- bios/settings/common.py
+++ bios/settings/common.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cms.context_processors.get_footer_description_list_context_processor'
             ],
         },
     },
