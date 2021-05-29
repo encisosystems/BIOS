975eecda1baacb6f464d06c63286b4ef8e520cfe
Alexis Acevedo <badzdiaz@gmail.com>
2021-05-29 10:31:06 -0500

    add testing to models image and carrousel

--- cms/tests/test_models.py
+++ cms/tests/test_models.py
@@ -23,3 +23,25 @@ class TestCarrousel(TestCase):
         self.carrousel.delete()
         self.assertNotIn(self.carrousel, self.queryset)
 
+
+class Image(TestCase):
+    
+    def setUp(self):
+        Image.objects.create(id=1,title='Main', alt='image 1', image = 'image1.png', carrousel = '1')
+        self.image= Image.objects.get(id=1)
+        self.queryset = Image.objects.all()
+
+    def test_query_image(self):
+        image = self.image.title
+        self.assertEqual(image, 'Main')
+
+    def test_update_image(self):
+        self.image.title = 'image1 Update'
+        self.image.alt = 'image 1 update'
+        self.image.alt = 'image1.png'
+        self.image.save()
+        self.assertEqual(self.image.title, 'image1 Update')
+
+    def test_delete_image(self):
+        self.image.delete()
+        self.assertNotIn(self.image, self.queryset)
