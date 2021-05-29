dbfa8b32ef40eb2ac36d669515eaae2c7a661096
Juan David Pareja Soto <parejajd@outlook.com>
2021-05-28 21:45:50 -0500

    Manipulators List

--- certificates/urls.py
+++ certificates/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from certificates.views import manipulator, medicalCertificate
+from certificates.views import manipulator
 from . import views
 
 
 urlpatterns = [
-    path('medical-certificate/', medicalCertificate, name="medical-certificate"),
+    path('<int:pk>/medical-certificate/', views.CertificateDoctorView.as_view(), name="medical-certificate"),
     path('<int:pk>/cma-carnet/', views.CarnetView.as_view(), name="cma-carnet"),
     path('<int:pk>/certificate-letter/', views.CertificateLetterView.as_view(), name="certificate-letter"),
     path('manipulator/', manipulator, name="manipulator"),
--- certificates/views.py
+++ certificates/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from .models import CertificateAssistance, CertificateDoctor, Manipulator, Official, Person
+from .models import CapacityCertificate, CertificateAssistance, CertificateDoctor, Manipulator, Official, Person
 from django.views import generic
 
-def medicalCertificate(request):
-    return render(request, "medical-certificate.html")
-
+class CertificateDoctorView(generic.DetailView):
+    model = CertificateDoctor
+    context_object_name = 'certificate'
+    template_name = 'medical-certificate.html'
 
 class CertificateLetterView(generic.DetailView):
     model = CertificateAssistance
@@ -16,16 +17,17 @@ class CarnetView(generic.DetailView):
     model = Person
     template_name = 'cma-carnet.html' 
 
-class ManipulatorView(generic.ListView):
-    model = Manipulator
-    template_name = 'manipulator.html'
-    context_object_name = 'latest_manipulator_list'
-    
-    def get_queryset(self):
-        return Person.objects.order_by('-city')[:5]
+#class ManipulatorView(generic.ListView):
+#    model = Manipulator
+#    template_name = 'manipulator.html'
+#    context_object_name = 'latest_manipulator_list'
+#    
+#    def get_queryset(self):
+#        return Person.objects.order_by('-city')[:5]
 
 def manipulator(request, article=None):    
-    certificatesDoctorList = CertificateDoctor.objects
-    certificatesAssistanceList = CertificateAssistance.objects
+    certificatesDoctorList = CertificateDoctor.objects.order_by('?')
+    certificatesAssistanceList = CertificateAssistance.objects.order_by('?')
+    carnetList = CapacityCertificate.objects.order_by('?')
     
-    return render(request, 'manipulator.html', {'medicalList': certificatesDoctorList, 'assistanceList': certificatesAssistanceList})
+    return render(request, 'manipulator.html', {'medicalList': certificatesDoctorList, 'assistanceList': certificatesAssistanceList, 'carnetList':carnetList})
