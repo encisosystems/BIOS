89cc92ed6dfe9c9f3848a0ae5b2aece76fd944ac
LinaMejia <linalmejia@gmail.com>
2021-05-29 11:02:15 -0500

    create test for Template Model

--- cms/models.py
+++ cms/models.py
@@ -12,25 +12,6 @@ class Image(models.Model):
     carrousel = models.ForeignKey(Carrousel, on_delete=models.CASCADE, null=True)
 
 
-class Template(models.Model):
-    icon = models.ImageField(
-        upload_to='cms/icons',
-    )
-    logo = models.ImageField(
-        upload_to='cms/logos',
-    )
-    header_title = models.CharField(
-        max_length=50
-    )
-
-    def __str__(self):
-        return f'{self.header_title}'
-
-    class Meta:
-        verbose_name = 'Template'
-        verbose_name_plural = 'Templates'
-
-
 class Page(models.Model):
     """Model Page"""
     title = models.CharField(
@@ -46,6 +27,38 @@ class Page(models.Model):
         ordering = ["id"]
         verbose_name = "Page"
         verbose_name_plural = "Pages"
+
+
+class Menu(models.Model):
+    # template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(
+        max_length=50
+    )
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+
+
+class Template(models.Model):
+    icon = models.ImageField(
+        upload_to='cms/icons',
+    )
+    logo = models.ImageField(
+        upload_to='cms/logos',
+    )
+    header_title = models.CharField(
+        max_length=50
+    )
+    menu_items = models.ManyToManyField(Menu)
+
+    def __str__(self):
+        return f'{self.header_title}'
+
+    class Meta:
+        verbose_name = 'Template'
+        verbose_name_plural = 'Templates'
 
 
 class Content(models.Model):
@@ -74,8 +87,3 @@ class FooterDescription(models.Model):
         null=True
     )
     template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
-
-
-class Menu(models.Model):
-    template = models.ForeignKey(Template, on_delete=models.SET_NULL, null=True, blank=True)
-    page = models.ForeignKey(Page, on_delete=models.CASCADE, null=True)
