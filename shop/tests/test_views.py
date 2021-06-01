275b7330701e1cbfee2977582a146f5f6ba2d0aa
root <root@DESKTOP-DRTB4S8.localdomain>
2021-05-31 23:41:46 -0500

    create a test for the views of Shop

--- shop/tests/test_views.py
+++ shop/tests/test_views.py
@@ -8,3 +8,9 @@ class TestShopViews(TestCase):
         response = client.get(reverse('checkout'))
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response,'checkout.html')
+    
+    def test_shop_GET(self):
+        client = Client()
+        response = client.get(reverse('shop'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'shop.html')
