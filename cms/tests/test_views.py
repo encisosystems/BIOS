d6fae0adca0e5a828d0e0d4472d017b6d3686372
romnyduarte <romnyduarte@outlook.com>
2021-06-04 22:24:10 -0500

    add test views cms

---
+++ cms/tests/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class TestViews(TestCase):
+    
+    def test_blog_GET(self):
+        client = Client()
+        response = client.get(reverse('blog'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response,'blog.html')
+
+
