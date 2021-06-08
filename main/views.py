1f2a6ce5eef9675f4b7fee91275b7c7fc65a21a2
dpalacios <diferpaho@gmail.com>
2021-06-08 08:43:50 -0500

    changes in base and home

--- main/views.py
+++ main/views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from cms.models import *
 
 def home(request):
-    images = Image.objects.all()
-    context={'images':images}
-    return render (request, 'home.html', context) 
+    return render(request, "home.html")
